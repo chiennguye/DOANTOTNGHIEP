7375c812b8aa322bedb3b4df991fab9f1c025575
chiennguye <chiennguyen11042003@gmail.com>
2025-05-15 10:52:35 +0700

    Update project configuration for deployment

---
+++ hhtlmilkteaapi/src/main/java/com/fpt/hhtlmilkteaapi/HealthController.java
@@ -0,0 +1,12 @@
+package com.fpt.hhtlmilkteaapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String home() {
+        return "OK";
+    }
+}
